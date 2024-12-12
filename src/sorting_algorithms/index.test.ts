4933712f8411bd952e5b02de833ab96e09e05d6d
pkolt <pkoltyshev@gmail.com>
2024-12-12 22:21:48 +0400

    Added tests

---
+++ src/sorting_algorithms/index.test.ts
@@ -0,0 +1,27 @@
+import { test } from 'node:test';
+import assert from 'node:assert';
+import { quickSort } from './quick_sort.js';
+import { bubbleSort } from './bubble_sort.js';
+import { selectionSort } from './selection_sort.js';
+
+function randomArray(size: number) {
+  return Array.from(Array(size), () => Math.floor(Math.random() * (size + 1)));
+}
+
+const ORIGIN_ARRAY: readonly number[] = Object.freeze(randomArray(10_000));
+const EXPECTED_ARRAY: readonly number[] = ORIGIN_ARRAY.toSorted((a, b) => a - b);
+
+test('quick sort', () => {
+  const result = quickSort(ORIGIN_ARRAY);
+  assert.deepEqual(result, EXPECTED_ARRAY);
+});
+
+test('selection sort', () => {
+  const result = selectionSort(ORIGIN_ARRAY);
+  assert.deepEqual(result, EXPECTED_ARRAY);
+});
+
+test('bubble sort', () => {
+  const result = bubbleSort(ORIGIN_ARRAY);
+  assert.deepEqual(result, EXPECTED_ARRAY);
+});
