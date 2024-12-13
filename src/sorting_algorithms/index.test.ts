828a6be650c10296247750c9aab911794fb30df6
pkolt <pkoltyshev@gmail.com>
2024-12-13 18:37:07 +0400

    Fixed typos

--- src/sorting_algorithms/index.test.ts
+++ src/sorting_algorithms/index.test.ts
@@ -3,6 +3,7 @@ import assert from 'node:assert';
 import { quickSort } from './quick_sort.js';
 import { bubbleSort } from './bubble_sort.js';
 import { selectionSort } from './selection_sort.js';
+import { insertionSort } from './insertion_sort.js';
 
 function randomArray(size: number) {
   return Array.from(Array(size), () => Math.floor(Math.random() * (size + 1)));
@@ -13,6 +14,11 @@ const EXPECTED_ARRAY: readonly number[] = ORIGIN_ARRAY.toSorted((a, b) => a - b)
 
 test('quick sort', () => {
   const result = quickSort(ORIGIN_ARRAY);
+  assert.deepEqual(result, EXPECTED_ARRAY);
+});
+
+test('insertion sort', () => {
+  const result = insertionSort(ORIGIN_ARRAY);
   assert.deepEqual(result, EXPECTED_ARRAY);
 });
 
